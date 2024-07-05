37f9a691b9a12b166547ec003f5a2fd78eb09b2f
julien pontoire <julien.pontoire@etu.utc.fr>
2024-07-05 10:13:57 +0200

    Initialisation du tableau foncionnelle
    Faire l'ajout des voltorbes dans une fonction à part

--- init-tab.sh
+++ init-tab.sh
@@ -1,25 +1,39 @@
 #!/bin/bash
 
-chr() {
-  [ "$1" -lt 256 ] || return 1
-  echo $(printf "\\$(printf '%03o' "$1")")
-}
-
 function init-tab {
 	n=5
+	voltorbe=0
 	for i in $(seq 1 $n)
 	do
 		for j in $(seq 1 $n)
 		do
 			lettre=$(printf "\\$(printf '%03o' "$(($j+64))")")
-			val=$((RANDOM%4))
-			touch "$lettre$i.sh"
-			# echo "$val"
+			val=$(($((RANDOM%3))+1))
+			echo "$val"
+			touch "coordo/$lettre$i.sh"
+			echo "$val" > "coordo/$lettre$i.sh"
 		done
 	done
 }
+
+
+
+
+
+
 
 init-tab
 
 
 # chr $(($j+64))
+#if [ $val -eq 0 ]
+#then
+#	voltorbe=$(($voltorbe+1))
+#fi
+#if [ $voltorbe -gt $1 ]
+#then
+#	while [ $val -eq 0 ]
+#	do
+#		val=$((RANDOM%4))
+#	done
+#fi
