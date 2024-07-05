1542a5e196fef289f2488774f5bb8b3a2aa161ed
Emmanuel D'Estaing <manue360@outlook.fr>
2024-07-05 11:20:45 +0200

    Assigner un nom

--- menu.sh
+++ menu.sh
@@ -6,11 +6,14 @@ echo "1. Facile"
 echo "2. Moyen"
 echo "3. Difficile"
 echo "Saisir 'Non' pour annuler"
-read  "Sélectionnez une difficulté: " choix
+read -p "Sélectionnez une difficulté: " choix
 case $choix in
  1) echo "5 5 3"; break ;;  # grille 5x5 avec 3 mines
  2) echo "5 5 4"; break ;;
  3) echo "5 5 5"; break ;;
  non) echo "partie annulée"
+esac
+done
 }
 
+Chosir_la_difficulte
