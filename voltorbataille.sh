25b17f1e33d617c9d6518034a873744de775bab7
Emmanuel D'Estaing <manue360@outlook.fr>
2024-07-05 10:24:17 +0200

    2 lignes modifiés

--- voltorbataille.sh
+++ voltorbataille.sh
@@ -1,10 +1,16 @@
 #Fonction pour choisir la difficulté
 
+Chosir_la_difficulte (){
+while true; do
 echo "1. Facile"
 echo "2. Moyen"
 echo "3. Difficile"
+echo "Saisir 'Non' pour annuler"
 read  "Sélectionnez une difficulté: " choix
 case $choix in
- 1) echo "5 5 3" ;;  # grille 5x5 avec 3 mines
- 2) echo "5 5 4" ;;
- 3) echo "5 5 5" ;;
+ 1) echo "5 5 3"; break ;;  # grille 5x5 avec 3 mines
+ 2) echo "5 5 4"; break ;;
+ 3) echo "5 5 5"; break ;;
+ non) echo "partie annulée"
+}
+
