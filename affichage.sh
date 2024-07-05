fab966390fc900ec7845062f111d9d3984ef52b8
Augustin Renaud <augustin.renaud@etu.utc.fr>
2024-07-05 10:06:25 +0200

    envoie de la taille

--- affichage.sh
+++ affichage.sh
@@ -2,21 +2,21 @@
 
 function affichage_ligne(
 	echo -n "+"
-	for i in $(seq 1 4)
+	for i in $(seq 1 $(("$1"-1)))
 	do
 		echo -n "---+"
 	done
 	echo "---+"
 )
 
-affichage_ligne 
-for  i in $(seq 1 5) 
+affichage_ligne "$1"
+for  i in $(seq 1 "$1") 
 do 
 	echo -n "|"
-	for j in $(seq 1 4)
+	for j in $(seq 1 $(("$1"-1)))
 	do
 		echo -n " O |"
 	done
 	echo  " O |"
-	affichage_ligne
+	affichage_ligne "$1"
 done	
