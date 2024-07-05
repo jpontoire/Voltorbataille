19f29a2a9e9f5c67b94bba679a29df9752a1985a
julien pontoire <julien.pontoire@etu.utc.fr>
2024-07-05 14:20:47 +0200

    Réparation fonction maj_etat_jeu

--- maj_etat_de_jeu.sh
+++ maj_etat_de_jeu.sh
@@ -1,4 +1,25 @@
+#!/bin/bash
+
 #Fonction pour mettre à jour l'état de jeu
+
+function maj-etat {
+	case="coordo/$1.sh"
+	val=$(cat $case)
+	#echo "$val"
+	if [ $val -eq 0 ]
+	then
+		echo "0"
+	else
+		echo $((-$val)) > $case
+	fi
+}
+
+#maj-etat $1
+
+
+
+
+
 
 maj_etat_de_jeu (){
 etat_jeu=$1
@@ -6,4 +27,4 @@ coordonnees=$2
 echo $etat_jeu
 }
 
-maj_etat_de_jeu
+#maj_etat_de_jeu
